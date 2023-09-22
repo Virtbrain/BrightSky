ac566c21adcfcc23b384b3a9fcbb13b3354dac45
AlexeyManankov <virtbrain@gmail.com>
2023-09-22 21:19:29 +0500

    same a s the last one

--- BrightSky/Views/SettingsView.swift
+++ BrightSky/Views/SettingsView.swift
@@ -8,15 +8,60 @@
 import UIKit
 
 final class SettingsView: UIView {
+    
+    private var viewModel: SettingsViewViewModel? {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .red
         translatesAutoresizingMaskIntoConstraints = false
+        addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    public func configure(with viewModel: SettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+}
+
+
+extension SettingsView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.options.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if let viewModel {
+            cell.textLabel?.text = viewModel.options[indexPath.row].title
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
